07754b620163cec0ca87871771f1eaac00c840f7
zxy2012 <617944292@qq.com>
2018-05-21 10:16:51 +0800

    project  commit in 21st

---
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -0,0 +1,82 @@
+package com.mmall.common;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.io.Serializable;
+
+/**
+ * Created by Xueyong on 2018/5/18.
+ */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+//保证序列化json的时候，如果是null的对象，key也会消失
+public class ServerResponse<T> implements Serializable{
+
+    private int status;
+    private String msg;
+    private T data;
+
+    private ServerResponse(int status){
+        this.status = status;
+    }
+    private ServerResponse(int status,T data){
+        this.status = status;
+        this.data = data;
+    }
+    private ServerResponse(int status,String msg,T data){
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+    private ServerResponse(int status,String msg){
+        this.status = status;
+        this.msg = msg;
+    }
+
+    @JsonIgnore
+    //使之不在json序列化结果当中
+    public boolean isSuccess(){
+        return this.status == ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getStatus(){
+        return status;
+    }
+    public T getData(){
+        return data;
+    }
+    public String getMsg(){
+        return msg;
+    }
+
+    //声明此方法持有一个类型T，也可以理解成，将此方法声明为泛型方法;
+    //第二个T就很容易理解啦，和List<T> 里面是一样的;
+    //第一个记住是持有，如果不持有，后面的ServerResponse<T>会报错
+    public static <T> ServerResponse<T> createBySuccess(){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    }
+    public static <T> ServerResponse<T> createBySuccess(String msg,T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    }
+
+    public static <T> ServerResponse<T> createByError(){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+    }
+
+    public static <T> ServerResponse<T> createByErrorMessage(String errormessage){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errormessage);
+    }
+
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode,String errorMessage){
+        return new ServerResponse<T>(errorCode,errorMessage);
+    }
+
+}
---
+++ src/main/java/com/mmall/controller/backend/productManageController.java
@@ -0,0 +1,7 @@
+package com.mmall.controller.backend;
+
+/**
+ * Created by Xueyong on 2018/5/21.
+ */
+public class ProductManageController {
+}
